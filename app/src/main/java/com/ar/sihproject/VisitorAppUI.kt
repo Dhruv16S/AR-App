5fe9e6bd76362e7290e09169729f3196e864d038
Dhruv Saxena <dhruv162002@gmail.com>
2022-03-18 15:50:10 +0530

    Complete Menu and Attachments

--- app/src/main/java/com/ar/sihproject/VisitorAppUI.kt
+++ app/src/main/java/com/ar/sihproject/VisitorAppUI.kt
@@ -12,6 +12,8 @@ import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.hitomi.cmlibrary.CircleMenu
 
 class VisitorAppUI : AppCompatActivity() {
@@ -22,24 +24,64 @@ class VisitorAppUI : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_visitor_app_ui)
 
+        val fragmentManager : FragmentManager = supportFragmentManager
+        val fragmentTransaction : FragmentTransaction = fragmentManager.beginTransaction()
+
+        val mapsFragment = Maps()
+        fragmentTransaction.add(R.id.frame, mapsFragment)
+
+        fragmentTransaction.commit()
+
         cmMain = findViewById(R.id.cmMain)
         cmMain.setMainMenu(Color.parseColor("#000000"), R.drawable.ic_menu, R.drawable.ic_cancel)
 
-        cmMain.addSubMenu(Color.parseColor("#00AF91"), R.drawable.ic_home)
-        cmMain.addSubMenu(Color.parseColor("#007965"), R.drawable.ic_favourite)
-        cmMain.addSubMenu(Color.parseColor("#F58634"), R.drawable.ic_contact)
-        cmMain.addSubMenu(Color.parseColor("#FFCC29"), R.drawable.ic_info)
+        cmMain.addSubMenu(Color.parseColor("#F5DE61"), R.drawable.ic_home)
+        cmMain.addSubMenu(Color.parseColor("#F56161"), R.drawable.ic_favourite)
+        cmMain.addSubMenu(Color.parseColor("#BBF561"), R.drawable.ic_contact)
+        cmMain.addSubMenu(Color.parseColor("#a0cbf1"), R.drawable.ic_info)
 
         //adding click listeners to menu items
         cmMain.setOnMenuSelectedListener { it ->
             when (it) {
-                0 -> Toast.makeText(this, "This is Home!", Toast.LENGTH_SHORT).show()
+                0 -> {
+                    //Toast.makeText(this, "This is Maps", Toast.LENGTH_SHORT).show()
+                    val fragmentManagerMaps : FragmentManager = supportFragmentManager
+                    val fragmentTransactionMaps : FragmentTransaction = fragmentManagerMaps.beginTransaction()
+                    val mapsFragmentOnClick = Maps()
+                    fragmentTransactionMaps.replace(R.id.frame, mapsFragmentOnClick)
+                    fragmentTransactionMaps.addToBackStack(null)
+                    fragmentTransactionMaps.commit()
+                }
 
-                1 -> Toast.makeText(this, "This is Favourite", Toast.LENGTH_SHORT).show()
+                1 -> {
+                    //Toast.makeText(this, "This is Favourites", Toast.LENGTH_SHORT).show()
+                    val fragmentManagerFavourites : FragmentManager = supportFragmentManager
+                    val fragmentTransactionFavourites : FragmentTransaction = fragmentManagerFavourites.beginTransaction()
+                    val favouritesFragment = Favourites()
+                    fragmentTransactionFavourites.replace(R.id.frame, favouritesFragment)
+                    fragmentTransactionFavourites.addToBackStack(null)
+                    fragmentTransactionFavourites.commit()
+                }
 
-                2 -> Toast.makeText(this, "This is Contact", Toast.LENGTH_SHORT).show()
+                2 -> {
+                    //Toast.makeText(this, "This is Account", Toast.LENGTH_SHORT).show()
+                    val fragmentManagerAccount : FragmentManager = supportFragmentManager
+                    val fragmentTransactionAccount : FragmentTransaction = fragmentManagerAccount.beginTransaction()
+                    val accountFragment = Account()
+                    fragmentTransactionAccount.replace(R.id.frame, accountFragment)
+                    fragmentTransactionAccount.addToBackStack(null)
+                    fragmentTransactionAccount.commit()
+                }
 
-                3 -> Toast.makeText(this, "This is About", Toast.LENGTH_SHORT).show()
+                3 -> {
+                    //Toast.makeText(this, "This is Scan and View", Toast.LENGTH_SHORT).show()
+                    val fragmentManagerScan : FragmentManager = supportFragmentManager
+                    val fragmentTransactionScan : FragmentTransaction = fragmentManagerScan.beginTransaction()
+                    val scanFragment = ScanAndView()
+                    fragmentTransactionScan.replace(R.id.frame, scanFragment)
+                    fragmentTransactionScan.addToBackStack(null)
+                    fragmentTransactionScan.commit()
+                }
             }
         }
     }
